8979142c247e1db8e708966c5dc7335f5ad36548
Meritxell García-Calvillo Jiménez <1hiaw.garciacjimenez.meritxell@gmail.com>
2022-02-10 18:23:45 +0100

    app modifyed2

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,50 @@
 <template>
-    <input placeholder="Deja tu comentario">
+    <input v-model="subcomentario" placeholder="Deja tu comentario" @keypress.enter="guardarMensaje">
+    
+    <button @click="cargarComentarios">Cargar ya!</button>
+    
+    <p v-for="comentario in comentarios" :key="comentario.id">
+        {{ comentario.texto }}
+    </p>
 </template>
 
 <script>
     export default {
-        name: "app"
+        name: "app",
+        data() {
+            return {
+                subcomentario: "holaaa",
+                comentarios: [
+                    { id: 1, texto: "adeu, molt bones" },
+                    { id: 2, texto: "holiwiii" },
+                    { id: 3, texto: "byeeee" },
+                    { id: 4, texto: "holaaas" }
+                ]
+            }
+        },
+        methods: {
+            guardarMensaje() {
+                this.comentarios.push({id: 5, texto: this.subcomentario})
+                //guardar en la base de datos
+            },
+            async cargarComentarios() {
+                //Descarreguem les dades de la bbdd
+                let respuesta = await fetch("/api/get");
+                //Pasem les dades a json
+                let respuestaJSON = await respuesta.json();
+                //enviem a la variable comentarios les dades aconseguides
+                this.comentarios = respuestaJSON;
+
+                //podem fer els 3 pasos en 1 d'aquesta manera:
+                //this.comentarios = await (await fetch("/api/get")).json();
+            }
+        }
     }
 </script>
 
 <style scoped>
     input {
-        color: rgb(0, 26, 255);
+        color:cornflowerblue;
         font-family: noteworthy;
     }
 </style>
